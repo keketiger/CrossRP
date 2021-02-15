5e02e66e8657f5300f234f47de458be8a268669c
Kevin <34973659+keketiger@users.noreply.github.com>
2021-02-15 14:12:00 +0100

    Update scoreboard_drawing.lua

--- addons/keketiger_system/lua/autorun/scoreboard_drawing.lua
+++ addons/keketiger_system/lua/autorun/scoreboard_drawing.lua
@@ -15,22 +15,22 @@ function ScoreboardDrawing.DrawOutlinedRect(x, y, w, h, t, c)
     surface.SetDrawColor(c)
 
     for i=0, t - 1 do
-		surface.DrawOutlinedRect(x + i, y + i, w - i * 2, h - i * 2)
+        surface.DrawOutlinedRect(x + i, y + i, w - i * 2, h - i * 2)
     end
 end
 
 local blur = Material("pp/blurscreen")
 function ScoreboardDrawing.BlurMenu(panel, layers, density, alpha)
-	local x, y = panel:LocalToScreen(0, 0)
+    local x, y = panel:LocalToScreen(0, 0)
 
-	surface.SetDrawColor(255, 255, 255, alpha)
-	surface.SetMaterial(blur)
+    surface.SetDrawColor(255, 255, 255, alpha)
+    surface.SetMaterial(blur)
 
-	for i=1, 3 do
-		blur:SetFloat("$blur", (i / layers) * density)
-		blur:Recompute()
+    for i=1, 3 do
+        blur:SetFloat("$blur", (i / layers) * density)
+        blur:Recompute()
 
-		render.UpdateScreenEffectTexture()
-		surface.DrawTexturedRect(-x, -y, ScrW(), ScrH())
-	end
+        render.UpdateScreenEffectTexture()
+        surface.DrawTexturedRect(-x, -y, ScrW(), ScrH())
+    end
 end
