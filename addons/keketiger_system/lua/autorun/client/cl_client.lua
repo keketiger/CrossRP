7ef9ee1d0fc7ca21ad77f9f74f2b0b2059b71751
Kevin <34973659+keketiger@users.noreply.github.com>
2021-02-15 14:36:09 +0100

    Update cl_client.lua

--- addons/keketiger_system/lua/autorun/client/cl_client.lua
+++ addons/keketiger_system/lua/autorun/client/cl_client.lua
@@ -1,8 +1,7 @@
 if (CLIENT) then
-	timer.Create("ClearDecals", 180, 0, function() -- 180 seconds
-		RunConsoleCommand("r_cleardecals", "") -- Command that clear decals
-		--notification.AddLegacy( "Successful cleaning decals on the map!", NOTIFY_GENERIC, 5 ) -- Message about success clear decals
-		-- RunConsoleCommand("cl_removedecals", "") -- Old command that working when sv_cheats 1
-		--print("[ACD] Successful cleaning decals on the map!") -- Print message in player console
-	end)	
+    timer.Create('ClearDecals', 180, 0, function() -- 180 seconds
+        RunConsoleCommand('r_cleardecals', '')
+        --RunConsoleCommand('cl_removedecals', '') -- Old command that working when sv_cheats 1
+        --print('[Serveur] Nettoyage des decals sur la carte réussi !') -- Print message in player console
+    end)
 end
