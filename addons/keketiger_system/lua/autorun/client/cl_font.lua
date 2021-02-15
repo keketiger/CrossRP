46c01418dd73c82d3ecedd1535116dcb4152172f
Kevin <34973659+keketiger@users.noreply.github.com>
2021-02-15 14:37:32 +0100

    Update cl_font.lua

--- addons/keketiger_system/lua/autorun/client/cl_font.lua
+++ addons/keketiger_system/lua/autorun/client/cl_font.lua
@@ -1,30 +1,31 @@
 if not Tiger.Font then
+    surface.CreateFont('TigerAS',{
+        font = 'Default',
+        size = 18,
+        bold = true,
+        underline = true,
+        italic = false
+    })
 
-	surface.CreateFont("TigerAS",{
-    font = "Default",
-    size = 18,
-    bold = true,
-    underline = true,
-    italic = false,
-	})
-	surface.CreateFont("TigerAS1",{
-		font = "Arial",
-		size = 18,
-		weight = 750,
-	})
-	surface.CreateFont("TigerAS2",{
-		font = "Default",
-		size = 24,
-		underline = true,
-		italic = false,
-		bold = true,
-	})
-	surface.CreateFont("TigerAS3",{
-		font = "Default",
-		size = 25,
-		underline = true,
-		italic = false,
-		bold = true,
-	})
-	
+    surface.CreateFont('TigerAS1',{
+        font = 'Arial',
+        size = 18,
+        weight = 750
+    })
+
+    surface.CreateFont('TigerAS2',{
+        font = 'Default',
+        size = 24,
+        underline = true,
+        italic = false,
+        bold = true
+    })
+
+    surface.CreateFont('TigerAS3',{
+        font = 'Default',
+        size = 25,
+        underline = true,
+        italic = false,
+        bold = true
+    })
 end
