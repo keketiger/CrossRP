44c24f65429fe2cbd0174dfa893cd339434a4d27
Kevin <34973659+keketiger@users.noreply.github.com>
2021-02-15 14:41:15 +0100

    Update cl_font_contextmenu.lua

--- addons/keketiger_system/lua/autorun/client/cl_font_contextmenu.lua
+++ addons/keketiger_system/lua/autorun/client/cl_font_contextmenu.lua
@@ -1,30 +1,31 @@
 if not Tiger.Font then
+    surface.CreateFont('TigerAS_Context',{
+        font = 'Default',
+        size = 18,
+        bold = true,
+        underline = true,
+        italic = false
+    })
 
-	surface.CreateFont("TigerAS_Context",{
-	    font = "Default",
-	    size = 18,
-	    bold = true,
-	    underline = true,
-	    italic = false,
-	})
-	surface.CreateFont("TigerAS1_Context",{
-		font = "Arial",
-		size = ScrH() * 0.017,
-		weight = 750,
-	})
-	surface.CreateFont("TigerAS2_Context",{
-		font = "Default",
-		size = ScrH() * 0.028,
-		underline = true,
-		italic = false,
-		bold = true,
-	})
-	surface.CreateFont("TigerAS3_Context",{
-		font = "Default",
-		size = 25,
-		underline = true,
-		italic = false,
-		bold = true,
-	})
-	
+    surface.CreateFont('TigerAS1_Context',{
+        font = 'Arial',
+        size = ScrH() * 0.017,
+        weight = 750
+    })
+
+    surface.CreateFont('TigerAS2_Context',{
+        font = 'Default',
+        size = ScrH() * 0.028,
+        underline = true,
+        italic = false,
+        bold = true
+    })
+
+    surface.CreateFont('TigerAS3_Context',{
+        font = 'Default',
+        size = 25,
+        underline = true,
+        italic = false,
+        bold = true
+    })
 end
